3823ecdb0c4ed936b0e5e38ff0f06fd3a1a1788b
ezgikardes <ezgikardes@gmail.com>
2024-09-27 19:48:32 +0200

    project done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Wall wall1 = new Wall("north");
+        Wall wall2 = new Wall("south");
+        Wall wall3 = new Wall("east");
+        Wall wall4 = new Wall("west");
+        Ceiling ceiling = new Ceiling(2, PaintColor.GREEN);
+        Carpet carpet = new Carpet(120, 80, PaintColor.WHITE);
+        Lamp lamp = new Lamp(LampType.NORMAL, true, 3);
+        Bed bed = new Bed("double", 2, 100, 2, 1);
+        Wardrobe wardrobe = new Wardrobe(1, 1, 10.0);
+
+        Bedroom bedroom = new Bedroom("child", wall1, wall2, wall3, wall4,
+                ceiling, bed, lamp, wardrobe, carpet);
+
+        System.out.println(bedroom);
     }
 }
